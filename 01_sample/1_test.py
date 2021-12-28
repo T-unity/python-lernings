e86ab937d4089ccda1976db54228fa3d3d479e2b
T-unity <takadanaritoshi@gmail.com>
2021-12-28 19:39:16 +0900

    関数を入れ子にした場合のスコープを確認

--- 01_sample/1_test.py
+++ 01_sample/1_test.py
@@ -1,1 +1,14 @@
 # coding: UTF-8
+
+context = 'グローバル'
+
+def check_scope_parent():
+  context = '親の関数'
+
+  def check_scope_child():
+    # context = '子の関数'
+    return context
+
+  return check_scope_child()
+
+print(check_scope_parent())
