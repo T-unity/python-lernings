6632137239021d0be351ef22013e96755312986f
T-unity <takadanaritoshi@gmail.com>
2021-12-29 19:59:51 +0900

    可変長引数

--- 01_sample/1_test.py
+++ 01_sample/1_test.py
@@ -1,11 +1,9 @@
 # coding: UTF-8
 TAX_RATE = 1.1
 
-def total_price(*values):
-  res = 1
-  for value in values:
-    res += value
+def name(prefix, suffix, *args):
+  res = prefix
+  res += '・'.join(args)
+  return res + suffix
 
-  return res * TAX_RATE
-
-print(total_price(2000, 1000, 3500, 2200))
+print(name('鈴木', '一郎', 'middle1', 'middle2'))
