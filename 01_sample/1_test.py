f5c63536338e57c5ce554ad80d16c7d7bb698762
T-unity <takadanaritoshi@gmail.com>
2021-12-30 22:07:30 +0900

    デコレーターを利用した高階関数を作成

--- 01_sample/1_test.py
+++ 01_sample/1_test.py
@@ -2,7 +2,7 @@
 
 def log_func(some):
   def inner(*args, **kwargs):
-    print('デコレーターを利用しない高階関数')
+    print('デコレーターを利用した高階関数')
     print('------------------')
     print(f'Name: {some.__name__}')
     print(f'Args: {args}')
@@ -11,8 +11,11 @@ def log_func(some):
     return some(*args, **kwargs)
   return inner
 
-def setter(x, y, m='bar', n='piyo'):
+@log_func
+def setter_func(x, y, m='bar', n='piyo'):
   print(f'setter: {x}-{y}/{m}-{n}')
 
-log_setter = log_func(setter)
-log_setter(2021,12, m='hello', n='world')
+setter_func(2021,12, m='hello', n='world')
+
+# log_setter = log_func(setter_func)
+# log_setter(2021,12, m='hello', n='world')
