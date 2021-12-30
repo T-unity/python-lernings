74e7d6905060fb0618576104b09099dc71742c3a
T-unity <takadanaritoshi@gmail.com>
2021-12-30 22:24:42 +0900

    モジュールファイルを直接呼び出し

--- 01_sample/1_test.py
+++ 01_sample/1_test.py
@@ -1,21 +1,4 @@
 # coding: UTF-8
 
-def log_func(some):
-  def inner(*args, **kwargs):
-    print('デコレーターを利用した高階関数')
-    print('------------------')
-    print(f'Name: {some.__name__}')
-    print(f'Args: {args}')
-    print(f'keywords: {kwargs}')
-    print('------------------')
-    return some(*args, **kwargs)
-  return inner
-
-@log_func
-def setter_func(x, y, m='bar', n='piyo'):
-  print(f'setter: {x}-{y}/{m}-{n}')
-
-setter_func(2021,12, m='hello', n='world')
-
-# log_setter = log_func(setter_func)
-# log_setter(2021,12, m='hello', n='world')
+if __name__ == '__main__':
+  print('モジュールファイルを直接呼び出し')
