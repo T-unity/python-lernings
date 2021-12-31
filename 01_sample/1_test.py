299daea2edd3a786cf9d0028522cb7967baeebaf
T-unity <takadanaritoshi@gmail.com>
2021-12-31 17:06:18 +0900

    シンプルなクラスを作成

--- 01_sample/1_test.py
+++ 01_sample/1_test.py
@@ -1,21 +1,8 @@
 # coding: UTF-8
-import asyncio
-import time
 
-async def lazy_process(process1, process2, final, sec):
-  print(f'{process1}')
-  await asyncio.sleep(sec)
-  print(f'{process2}')
-  return f'{final}/{sec}'
+class Person:
+  pass
 
-# print(lazy_process('hey', 5))
-
-start = time.time()
-loop = asyncio.get_event_loop()
-result = loop.run_until_complete(
-  lazy_process('GO!!!', 'Finished!!!', 'success!!!', 5)
-)
-end = time.time()
-
-print(result)
-print(f'Process Time: {end - start}')
+if __name__ == '__main__':
+  p = Person()
+  print(p)
