3ca6d8d6b3d1eb9e7a656ea73a64edccde1ac9de
T-unity <takadanaritoshi@gmail.com>
2021-12-31 12:00:43 +0900

    イベントループを利用してコルーチンを実行

--- 01_sample/1_test.py
+++ 01_sample/1_test.py
@@ -1,6 +1,6 @@
 # coding: UTF-8
-
 import asyncio
+import time
 
 async def lazy_process(name, sec):
   print(f'start {name}')
@@ -8,4 +8,14 @@ async def lazy_process(name, sec):
   print(f'end {name}')
   return f'{name}/{sec}'
 
-print(lazy_process('hey', 5))
+# print(lazy_process('hey', 5))
+
+start = time.time()
+loop = asyncio.get_event_loop()
+result = loop.run_until_complete(
+  lazy_process('hello', 5)
+)
+end = time.time()
+
+print(result)
+print(f'Process Time: {end - start}')
