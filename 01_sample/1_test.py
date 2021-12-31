23233387888b8e4ad12d683aa9b6bbbdb5eae988
T-unity <takadanaritoshi@gmail.com>
2021-12-31 12:20:14 +0900

    コルーチンに渡す引数を明示的な引数に変更

--- 01_sample/1_test.py
+++ 01_sample/1_test.py
@@ -2,18 +2,18 @@
 import asyncio
 import time
 
-async def lazy_process(name, sec):
-  print(f'start {name}')
+async def lazy_process(process1, process2, final, sec):
+  print(f'{process1}')
   await asyncio.sleep(sec)
-  print(f'end {name}')
-  return f'{name}/{sec}'
+  print(f'{process2}')
+  return f'{final}/{sec}'
 
 # print(lazy_process('hey', 5))
 
 start = time.time()
 loop = asyncio.get_event_loop()
 result = loop.run_until_complete(
-  lazy_process('hello', 5)
+  lazy_process('GO!!!', 'Finished!!!', 'success!!!', 5)
 )
 end = time.time()
 
