3a0b1733b45d74680ec5ad106b11a6d40a2154d7
T-unity <takadanaritoshi@gmail.com>
2021-12-31 11:53:11 +0900

    簡易的なコルーチンを作成

--- 01_sample/1_test.py
+++ 01_sample/1_test.py
@@ -1,4 +1,11 @@
 # coding: UTF-8
 
-if __name__ == '__main__':
-  print('モジュールファイルを直接呼び出し')
+import asyncio
+
+async def lazy_process(name, sec):
+  print(f'start {name}')
+  await asyncio.sleep(sec)
+  print(f'end {name}')
+  return f'{name}/{sec}'
+
+print(lazy_process('hey', 5))
